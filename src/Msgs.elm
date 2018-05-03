977f1a9bc2dc2e358496a27673f4e645b5df8cb6
Peter Balogh <pbalogh@rethinkrobotics.com>
2018-05-03 13:56:33 -0400

    works with updates

--- src/Msgs.elm
+++ src/Msgs.elm
@@ -3,7 +3,10 @@ module Msgs exposing (..)
 import Models exposing (Player)
 import RemoteData exposing (WebData)
 import Navigation exposing (Location)
+import Http
 
 type Msg
   = OnFetchPlayers (WebData (List Player))
   | OnLocationChange Location
+  | ChangeLevel Player Int
+  | OnPlayerSave (Result Http.Error Player)
